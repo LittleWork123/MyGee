6dd981cf5e042739b39f77bf56c5c468fec557aa
LittleWorl123 <worldpeacedel@gmail.com>
2023-04-22 00:24:21 +0800

    1.独立出了Router 2.设计Context,封装Request和Response,提供了一些针对于Context的函数，例如PostForm和Query，提供对JSON，HTML数据类型的支持

--- gee/gee.go
+++ gee/gee.go
@@ -1,50 +1,40 @@
 package gee
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "net/http"
 
-// 定义处理Request的句柄函数
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
 
+// Engine implement the interface of ServeHTTP
 type Engine struct {
-	routerGroup map[string]HandlerFunc
+	router *router
 }
 
-// New 一个Engine
+// New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{routerGroup: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) addRoute(method string, pattern string, Handler HandlerFunc) {
-	key := method + "-" + pattern
-	if _, existed := engine.routerGroup[key]; existed {
-		log.Fatal("the method and pattern you added already existed")
-	}
-	engine.routerGroup[key] = Handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if Handler, ok := engine.routerGroup[key]; ok {
-		Handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND. %s \n", req.URL)
-	}
-}
-
-// GET 方法调用
-func (engine *Engine) GET(pattern string, Handler HandlerFunc) {
-	engine.addRoute("GET", pattern, Handler)
-}
-
-// POST 方法调用
-func (engine *Engine) POST(pattern string, Handler HandlerFunc) {
-	engine.addRoute("POST", pattern, Handler)
-}
-
-func (engine *Engine) Run(addr string) error {
-	return http.ListenAndServe(addr, engine)
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
